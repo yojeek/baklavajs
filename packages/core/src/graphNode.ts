d2cb9e3e7ab27cda92f6d5cf5722f32a76d50fa6
yojeek <antiplaka@gmail.com>
2023-11-05 18:53:14 +0100

    Export AbstractGraphNode as class from graphNode.ts

--- packages/core/src/graphNode.ts
+++ packages/core/src/graphNode.ts
@@ -18,156 +18,157 @@ export function getGraphNodeTypeString(template: GraphTemplate): string {
     return GRAPH_NODE_TYPE_PREFIX + template.id;
 }
 
-export function createGraphNodeType(template: GraphTemplate): new () => AbstractNode & IGraphNode {
-    return class GraphNode extends AbstractNode implements IGraphNode {
-        public type = getGraphNodeTypeString(template);
+export abstract class AbstractGraphNode extends AbstractNode implements IGraphNode {
+    public abstract template: GraphTemplate;
 
-        public override get title() {
-            return this._title;
+    public override get title() {
+        return this._title;
+    }
+
+    public override set title(v: string) {
+        this.template.name = v;
+    }
+
+    public inputs: Record<string, NodeInterface<any>> = {};
+    public outputs: Record<string, NodeInterface<any>> = {};
+
+    public subgraph: Graph | undefined;
+
+    public override calculate: CalculateFunction<Record<string, any>, Record<string, any>> = async (
+        inputs,
+        context,
+    ) => {
+        if (!this.subgraph) {
+            throw new Error(`GraphNode ${this.id}: calculate called without subgraph being initialized`);
         }
-        public override set title(v: string) {
-            this.template.name = v;
-        }
 
-        public inputs: Record<string, NodeInterface<any>> = {};
-        public outputs: Record<string, NodeInterface<any>> = {};
+        if (typeof context.engine === "object" && !!context.engine && typeof context.engine.runGraph === "function") {
+            const graphInputs: Map<string, any> = new Map();
 
-        public template = template;
-        public subgraph: Graph | undefined;
-
-        public override calculate: CalculateFunction<Record<string, any>, Record<string, any>> = async (
-            inputs,
-            context,
-        ) => {
-            if (!this.subgraph) {
-                throw new Error(`GraphNode ${this.id}: calculate called without subgraph being initialized`);
-            }
-
-            if (
-                typeof context.engine === "object" &&
-                !!context.engine &&
-                typeof context.engine.runGraph === "function"
-            ) {
-                const graphInputs: Map<string, any> = new Map();
-
-                // gather all values of the unconnected inputs
-                for (const n of this.subgraph.nodes) {
-                    Object.values(n.inputs).forEach((ni) => {
-                        if (ni.connectionCount === 0) {
-                            graphInputs.set(ni.id, ni.value);
-                        }
-                    });
-                }
-
-                // map graph inputs to their respective nodeInterfaceId in the graph
-                Object.entries(inputs).forEach(([k, v]) => {
-                    const gi = this.subgraph!.inputs.find((x) => x.id === k)!;
-                    graphInputs.set(gi.nodeInterfaceId, v);
+            // gather all values of the unconnected inputs
+            for (const n of this.subgraph.nodes) {
+                Object.values(n.inputs).forEach((ni) => {
+                    if (ni.connectionCount === 0) {
+                        graphInputs.set(ni.id, ni.value);
+                    }
                 });
-
-                const result: Map<string, Map<string, any>> = await context.engine.runGraph(
-                    this.subgraph,
-                    graphInputs,
-                    context.globalValues,
-                );
-                const flatResult: Map<string, any> = new Map();
-                result.forEach((nodeOutputs, nodeId) => {
-                    const node = this.subgraph!.nodes.find((n) => n.id === nodeId)!;
-                    nodeOutputs.forEach((v, nodeInterfaceKey) => {
-                        flatResult.set(node.outputs[nodeInterfaceKey].id, v);
-                    });
-                });
-
-                const outputs: Record<string, any> = {};
-                this.subgraph.outputs.forEach((graphOutput) => {
-                    outputs[graphOutput.id] = flatResult.get(graphOutput.nodeInterfaceId);
-                });
-
-                outputs._calculationResults = result;
-
-                return outputs;
             }
-        };
 
-        public override load(state: IGraphNodeState) {
-            if (!this.subgraph) {
-                throw new Error("Cannot load a graph node without a graph");
-            }
-            if (!this.template) {
-                throw new Error("Unable to load graph node without graph template");
-            }
-            this.subgraph.load(state.graphState);
-            super.load(state);
-        }
-
-        public override save(): IGraphNodeState {
-            if (!this.subgraph) {
-                throw new Error("Cannot save a graph node without a graph");
-            }
-            const state = super.save();
-            return {
-                ...state,
-                graphState: this.subgraph.save(),
-            };
-        }
-
-        public override onPlaced() {
-            this.template.events.updated.subscribe(this, () => this.initialize());
-            this.template.events.nameChanged.subscribe(this, (name) => {
-                this._title = name;
+            // map graph inputs to their respective nodeInterfaceId in the graph
+            Object.entries(inputs).forEach(([k, v]) => {
+                const gi = this.subgraph!.inputs.find((x) => x.id === k)!;
+                graphInputs.set(gi.nodeInterfaceId, v);
             });
-            this.initialize();
+
+            const result: Map<string, Map<string, any>> = await context.engine.runGraph(
+                this.subgraph,
+                graphInputs,
+                context.globalValues,
+            );
+            const flatResult: Map<string, any> = new Map();
+            result.forEach((nodeOutputs, nodeId) => {
+                const node = this.subgraph!.nodes.find((n) => n.id === nodeId)!;
+                nodeOutputs.forEach((v, nodeInterfaceKey) => {
+                    flatResult.set(node.outputs[nodeInterfaceKey].id, v);
+                });
+            });
+
+            const outputs: Record<string, any> = {};
+            this.subgraph.outputs.forEach((graphOutput) => {
+                outputs[graphOutput.id] = flatResult.get(graphOutput.nodeInterfaceId);
+            });
+
+            outputs._calculationResults = result;
+
+            return outputs;
+        }
+    };
+
+    public override load(state: IGraphNodeState) {
+        if (!this.subgraph) {
+            throw new Error("Cannot load a graph node without a graph");
+        }
+        if (!this.template) {
+            throw new Error("Unable to load graph node without graph template");
+        }
+        this.subgraph.load(state.graphState);
+        super.load(state);
+    }
+
+    public override save(): IGraphNodeState {
+        if (!this.subgraph) {
+            throw new Error("Cannot save a graph node without a graph");
+        }
+        const state = super.save();
+        return {
+            ...state,
+            graphState: this.subgraph.save(),
+        };
+    }
+
+    public override onPlaced() {
+        this.template.events.updated.subscribe(this, () => this.initialize());
+        this.template.events.nameChanged.subscribe(this, (name) => {
+            this._title = name;
+        });
+        this.initialize();
+    }
+
+    public override onDestroy() {
+        this.template.events.updated.unsubscribe(this);
+        this.template.events.nameChanged.unsubscribe(this);
+        this.subgraph?.destroy();
+    }
+
+    private initialize() {
+        if (this.subgraph) {
+            this.subgraph.destroy();
+        }
+        this.subgraph = this.template.createGraph();
+        this._title = this.template.name;
+        this.updateInterfaces();
+        this.events.update.emit(null);
+    }
+
+    private updateInterfaces() {
+        if (!this.subgraph) {
+            throw new Error("Trying to update interfaces without graph instance");
         }
 
-        public override onDestroy() {
-            this.template.events.updated.unsubscribe(this);
-            this.template.events.nameChanged.unsubscribe(this);
-            this.subgraph?.destroy();
+        for (const graphInput of this.subgraph.inputs) {
+            if (!(graphInput.id in this.inputs)) {
+                this.addInput(graphInput.id, new NodeInterface(graphInput.name, undefined));
+            } else {
+                this.inputs[graphInput.id].name = graphInput.name;
+            }
+        }
+        for (const k of Object.keys(this.inputs)) {
+            if (!this.subgraph.inputs.some((gi) => gi.id === k)) {
+                this.removeInput(k);
+            }
         }
 
-        private initialize() {
-            if (this.subgraph) {
-                this.subgraph.destroy();
+        for (const graphOutput of this.subgraph.outputs) {
+            if (!(graphOutput.id in this.outputs)) {
+                this.addOutput(graphOutput.id, new NodeInterface(graphOutput.name, undefined));
+            } else {
+                this.outputs[graphOutput.id].name = graphOutput.name;
             }
-            this.subgraph = this.template.createGraph();
-            this._title = this.template.name;
-            this.updateInterfaces();
-            this.events.update.emit(null);
+        }
+        for (const k of Object.keys(this.outputs)) {
+            if (!this.subgraph.outputs.some((gi) => gi.id === k)) {
+                this.removeOutput(k);
+            }
         }
 
-        private updateInterfaces() {
-            if (!this.subgraph) {
-                throw new Error("Trying to update interfaces without graph instance");
-            }
+        // Add an internal output to allow accessing the calculation results of nodes inside the graph
+        this.addOutput("_calculationResults", new NodeInterface("_calculationResults", undefined).setHidden(true));
+    }
+}
 
-            for (const graphInput of this.subgraph.inputs) {
-                if (!(graphInput.id in this.inputs)) {
-                    this.addInput(graphInput.id, new NodeInterface(graphInput.name, undefined));
-                } else {
-                    this.inputs[graphInput.id].name = graphInput.name;
-                }
-            }
-            for (const k of Object.keys(this.inputs)) {
-                if (!this.subgraph.inputs.some((gi) => gi.id === k)) {
-                    this.removeInput(k);
-                }
-            }
-
-            for (const graphOutput of this.subgraph.outputs) {
-                if (!(graphOutput.id in this.outputs)) {
-                    this.addOutput(graphOutput.id, new NodeInterface(graphOutput.name, undefined));
-                } else {
-                    this.outputs[graphOutput.id].name = graphOutput.name;
-                }
-            }
-            for (const k of Object.keys(this.outputs)) {
-                if (!this.subgraph.outputs.some((gi) => gi.id === k)) {
-                    this.removeOutput(k);
-                }
-            }
-
-            // Add an internal output to allow accessing the calculation results of nodes inside the graph
-            this.addOutput("_calculationResults", new NodeInterface("_calculationResults", undefined).setHidden(true));
-        }
+export function createGraphNodeType(template: GraphTemplate): new () => AbstractNode & IGraphNode {
+    return class GraphNode extends AbstractGraphNode implements IGraphNode {
+        public type = getGraphNodeTypeString(template);
+        public template = template;
     };
 }
