9c4969296e3291c1767c41dc9c9d11940dffc617
deusmax <deusmaxbiz@gmail.com>
2024-02-06 19:42:47 +0100

    Returned Save icon

--- packages/renderer-vue/src/icons/index.ts
+++ packages/renderer-vue/src/icons/index.ts
@@ -6,4 +6,5 @@ export { default as Copy } from "./Copy.vue";
 export { default as DeviceFloppy } from "./DeviceFloppy.vue";
 export { default as Hierarchy2 } from "./Hierarchy2.vue";
 export { default as VerticalDots } from "./VerticalDots.vue";
+export { default as Save } from "./Save.vue";
 export { default as TrashBin } from "./TrashBin.vue";
